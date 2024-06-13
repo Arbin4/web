6a8b18d561cc8787c4afd885834713a0318468b2
Arabinda Sigdel <arbsigdel2060@gmail.com>
2024-06-13 11:22:59 +0545

    Class

---
+++ script.js
@@ -0,0 +1,33 @@
+let number = 1;
+let num2 = 1.2;
+
+// console.log(number);
+// console.log(num2);
+
+num2 = "number";
+// console.log(num2);
+
+// let gods = ["ram", "shyam", "krishna"];
+// let weapons = ["bow", "arrow", "sudarsan"];
+
+// for (let item of gods) {
+//   //   console.log(item);
+
+//   for (var item of weapons) {
+//     // console.log(weapon);
+//   }
+//   //   console.log(item);
+// }
+
+const myName = "Aaalu";
+
+// myName = "Pidaalu";
+
+// console.log(myName);
+
+const myArr = [1, 2, 3];
+myArr[1] = 3;
+myArr.push(7);
+console.log(myArr);
+myArr.pop();
+console.log(myArr);
