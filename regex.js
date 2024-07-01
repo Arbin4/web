16ef6c153fde0dff616e66aa6f83e9ca20e0adac
Arabinda Sigdel <arbsigdel2060@gmail.com>
2024-07-01 12:51:08 +0545

    123

--- regex.js
+++ regex.js
@@ -6,7 +6,7 @@
 // const str = "Hello apple, \\ ball, car_123@ \n. cabc";
 // const regex = new RegExp(/\S/g);
 // const str = "2024-06-23";
-const str = "foobarbarfoo";
+// const str = "foobarbarfoo";
 // const regex = new RegExp(/[abc]/g);
 // const regex = new RegExp(/[a-e]/g);
 //Everything except a to e
@@ -16,7 +16,13 @@ const str = "foobarbarfoo";
 // const regex = new RegExp(/\Bppl\B/g);
 // const regex = new RegExp(/ab|cd/g);
 // const regex = new RegExp(/(\d{4})-(\d{2})-(\d{2})/);
-const regex = new RegExp(/(foo)(bar)\2\1/);
+// const regex = new RegExp(/(foo)(bar)\2\1/);
 // const reformattedDate = str.replace(regex, "$3/$2/$1");
+// console.log(str.match(regex));
+// console.log(reformattedDate)
+// let str = "redredapple";
+let str = "I 90 have 100 dollars";
+let regex = /\d+(?! dollars)/;
+// let regex2 = /(?:red)apple/;
+
 console.log(str.match(regex));
-// console.log(reformattedDate);
